bee1605bd783205b3fee444a2db0b8a0894bd88c
Peter <peter.prentiss@gmail.com>
2018-12-05 20:30:25 -0600

    Setup email and password inputs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 export default class LoginForm extends Component {
-    state = { text: '' }
+    state = { email: '', password: '' }
 
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput 
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
-                        style={{ height: 20, width: 100 }}
+                    <Input 
+                        placeholder="user@gmail.com"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ text })}
                     />
                 </CardSection>
-                <CardSection />
+
+                <CardSection>
+                    <Input
+                        placeholder="password"
+                        label="password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
                 <CardSection>
                     <Button>
                         Log In
